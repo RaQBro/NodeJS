8e77395b1518b3f3b97acd9275a2067ca168fa62
DESKTOP-MUQU1QE\RaQ <radu.danila@gmail.com>
2017-07-24 12:17:08 +0300

    Express get post get with custom path

--- ExpressHelloWorld/index.js
+++ ExpressHelloWorld/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 var iPort = "8081"
  
-app.get('/', function (req, res) {
+
+//Change route to respode to http://127.0.0.1:8081/hello/world
+app.get('/hello/world', function (req, res) {
   res.send('Hello World');
+})
+
+app.post('/', function (req, res) {
+  res.send('No post method available');
 })
  
 app.listen(iPort);
