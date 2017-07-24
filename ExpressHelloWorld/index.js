829b79120edb2b7fcc96cd190f5b3223f9ed323e
DESKTOP-MUQU1QE\RaQ <radu.danila@gmail.com>
2017-07-24 11:55:10 +0300

    Hello wordl with express

--- ExpressHelloWorld/index.js
+++ ExpressHelloWorld/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var iPort = "8081"
  
 app.get('/', function (req, res) {
   res.send('Hello World');
 })
  
-app.listen(3000);
+app.listen(iPort);
+
+console.log("App running http://127.0.0.1:" + iPort + "/");
