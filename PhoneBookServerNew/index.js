b340e5984f016be501ff271277d74b61ed906693
DESKTOP-MUQU1QE\RaQ <radu.danila@gmail.com>
2017-07-25 22:31:57 +0300

    no message

--- PhoneBookServerNew/index.js
+++ PhoneBookServerNew/index.js
@@ -7,12 +7,15 @@ var iPort = "8081"
 
 //Change route to respode to http://127.0.0.1:8081/hello/world
 oApp.get('/', function (req, res) {
-    var pathname = url.parse(req.url).pathname;
+    var oUrl = url.parse(req.url);
+    var pathname = oUrl.pathname;
     console.log("Request for " + pathname + " received.");
-    if (pathname == '/') {
-        var vFile = 'index.html'
-    } else {
-        var vFile = pathname.substr(1);
+    if (pathname == '/' && oUrl.query == null) {
+        var vFile = 'index.html';
+    }
+    if (oUrl.query != null) {
+        addPerson(oUrl.query)
+
     }
     mFs.readFile(vFile, function (err, data) {
         if (err) {
@@ -60,3 +63,29 @@ oApp.get('/style.css', function (req, res) {
 oApp.listen(iPort);
 
 console.log("App running http://127.0.0.1:" + iPort + "/");
+
+function addPerson(oPerson) {
+    var obj = {
+        persons: []
+    };
+
+    var json = JSON.stringify(obj);
+
+    mFs.writeFile('persons.json', json, 'utf8', callback);
+
+    mFs.readFile('persons.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+    obj.persons.push({
+        nume: oPerson.nume,
+        prenume: oPerson.prenume,
+        numar: oPerson.numar,
+        oras: oPerson.oras,
+        email: oPerson.email
+    });
+    json = JSON.stringify(obj); //convert it back to json
+    fs.writeFile('persons.json', json, 'utf8', callback); // write it back 
+}});
+}
