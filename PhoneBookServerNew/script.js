1f465412d299fea456faefbb37ddd4c73643f372
DESKTOP-MUQU1QE\RaQ <radu.danila@gmail.com>
2017-07-25 20:33:45 +0300

    working

---
+++ PhoneBookServerNew/script.js
@@ -0,0 +1,11 @@
+function add_person() {
+    var response = httpGet("127.0.0.1:8081/add");
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
