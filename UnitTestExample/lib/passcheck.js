31a79a643ab214ce66013ab999c7f33af5c3b863
DESKTOP-MUQU1QE\RaQ <radu.danila@gmail.com>
2017-07-25 13:29:18 +0300

    no message

--- UnitTestExample/lib/passcheck.js
+++ UnitTestExample/lib/passcheck.js
@@ -1,3 +1,9 @@
 exports.checkpass = module.exports.checkpass = function (password) {
+    if (password.length < 8) {
+        return false;
+    }
+    if (password.indexOf("password") >= 0) {
+        return false;
+    }
     return true;
 }
